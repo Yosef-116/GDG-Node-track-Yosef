6f84b92fb50f0aa5e6d4f910b2a32dbf59cbba2b
Yosef Hassen <120139960+Yosef-116@users.noreply.github.com>
2026-02-07 22:25:00 +0300

    Delete Task-3 directory

--- Task-3/src/utils/validationSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const bookSchema = joi.object({
-  title: joi.string().min(3).max(100).required(),
-  author: joi.string().min(3).max(50).required(),
-  price: joi.number().positive().required()
-});
-
-export { bookSchema };
